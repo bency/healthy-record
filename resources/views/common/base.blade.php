870a76873d8c53b5b937c392a54ff28577de8e47
bency <bency80097@gmail.com>
2016-03-05 20:56:07 +0800

    載入 sweet alert lib

--- resources/views/common/base.blade.php
+++ resources/views/common/base.blade.php
@@ -2,6 +2,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="{{asset('/css/sweet-alert.css')}}">
         <link rel="stylesheet" href="{{asset('/css/app.css')}}">
         <link rel="stylesheet" href="{{asset('/css/app.css.map')}}">
         <meta name="csrf-token" content="{{ csrf_token() }}">
@@ -65,6 +66,7 @@
         </div>
         <script src="{{ asset('/js/jquery.min.js') }}"></script>
         <script src="{{ asset('/js/bootstrap.min.js')}}"></script>
+        <script src="{{ asset('/js/sweet-alert.min.js') }}"></script>
         @yield('bottom-script')
     </body>
 </html>

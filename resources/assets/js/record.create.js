873c9ceca52dc29a855ec9e11b019b362e76aae1
bency <bency80097@gmail.com>
2016-03-05 21:12:31 +0800

    加入 record.create js

---
+++ resources/assets/js/record.create.js
@@ -0,0 +1,33 @@
+$.ajaxSetup({
+    headers: {
+        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+    }
+});
+
+var showResult = function (ret) {
+    console.log(ret);
+}
+
+var validate = function (type) {
+    var availableTypes = ['piss', 'pupu', 'food', 'water'];
+    if (-1 == availableTypes.indexOf(type)) {
+        return false;
+    }
+    return true;
+}
+var updateProfile = function () {
+    var type = $(this).data('type');
+    var data = {};
+    data.type = type;
+    if ("piss" == type || "water" == type) {
+        data.value = $('#' + type).val();
+    } else if ('food' == type) {
+        data.before = $('#before-eat').val();
+        data.after = $('#after-eat').val();
+    }
+    if (!validate(data.type)) {
+        return false;
+    }
+    $.post('/record', {type: data}).done(showResult);
+}
+$(document).on('click', "button", updateProfile);

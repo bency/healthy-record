93ee532d5e348dd6d4e5ad6c762cc775efe96c96
bency <bency80097@gmail.com>
2016-03-06 02:16:03 +0800

    限定只能輸入數字

--- resources/views/record/create.blade.php
+++ resources/views/record/create.blade.php
@@ -4,7 +4,7 @@
     <div class="form-group">
         <label class="col-xs-2 control-label" for="">小便</label>
         <div class="col-xs-8">
-            <input id="piss" type="number" class="form-control" name="piss" placeholder="小便容量">
+            <input id="piss" type="number" class="form-control" name="piss" pattern="\d*" placeholder="小便容量">
         </div>
         <button type="button" class="btn btn-default col-xs-2" data-function="submit" data-type="piss">送出</button>
     </div>
@@ -13,7 +13,7 @@
     <div class="form-group">
         <label class="col-xs-2 control-label" for="">喝水</label>
         <div class="col-xs-8">
-            <input id="water" type="number" class="form-control" name="water" placeholder="喝水容量">
+            <input id="water" type="number" class="form-control" name="water" pattern="\d*" placeholder="喝水容量">
         </div>
         <button type="button" class="btn btn-default col-xs-2" data-function="submit" data-type="water">送出</button>
     </div>
@@ -22,10 +22,10 @@
     <div class="form-group">
         <label class="col-xs-2 control-label" for="">進食</label>
         <div class="col-xs-4">
-            <input id="before-eat" type="number" class="form-control" name="before-eat" placeholder="進食前總重">
+            <input id="before-eat" type="number" class="form-control" name="before-eat" pattern="\d*" placeholder="進食前總重">
         </div>
         <div class="col-xs-4">
-            <input id="after-eat" type="number" class="form-control" name="after-eat" placeholder="進食後總重">
+            <input id="after-eat" type="number" class="form-control" name="after-eat" pattern="\d*" placeholder="進食後總重">
         </div>
         <button type="button" class="btn btn-default col-xs-2" data-function="submit" data-type="food">送出</button>
     </div>

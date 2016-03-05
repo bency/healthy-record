ad84be9822fa789ee8c127a224873eed68280304
bency <bency80097@gmail.com>
2016-03-06 00:38:32 +0800

    把常數搬到 Absort 去

--- app/Absort.php
+++ app/Absort.php
@@ -17,4 +17,25 @@ class Absort extends Model
     protected $dateFormat = 'U';
     protected $hidden = ['deleted_at'];
     protected $dates = ['deleted_at'];
+
+    public static $record_map = [
+        'water' => 1,
+        'food'  => 2,
+        'piss'  => 101,
+        'pupu'  => 102,
+    ];
+
+    public static $record_text = [
+        'water' => '喝水',
+        'food'  => '吃東西',
+        'piss'  => '小號',
+        'pupu'  => '大號',
+    ];
+
+    public static $unit = [
+        'water' => '公克',
+        'food'  => '公克',
+        'piss'  => '公克',
+        'pupu'  => '次',
+    ];
 }
--- app/Services/Record.php
+++ app/Services/Record.php
@@ -6,27 +6,6 @@ use App\Absort;
 
 class Record
 {
-    public static $record_map = [
-        'water' => 1,
-        'food'  => 2,
-        'piss'  => 101,
-        'pupu'  => 102,
-    ];
-
-    public static $record_text = [
-        'water' => '喝水',
-        'food'  => '吃東西',
-        'piss'  => '小號',
-        'pupu'  => '大號',
-    ];
-
-    public static $unit = [
-        'water' => '公克',
-        'food'  => '公克',
-        'piss'  => '公克',
-        'pupu'  => '次',
-    ];
-
     public static $accept_types = [
         'water',
         'food',
@@ -48,7 +27,7 @@ class Record
             $absort = 1;
         }
         $value = $data['value'];
-        $attribute_id = self::$record_map[$type];
+        $attribute_id = Absort::$record_map[$type];
         return self::add([
             'value' => $value,
             'absort' => $absort,
@@ -61,12 +40,12 @@ class Record
         if (!in_array($type, self::$accept_types)) {
             throw new Exception('無此類型的紀錄');
         }
-        $attribute_id = self::$record_map[$type];
+        $attribute_id = Absort::$record_map[$type];
         $sum = Absort::where('attribute_id', $attribute_id)
             ->where('created_at', '>', strtotime('today'))
             ->where('created_at', '<=', strtotime('tomorrow'))
             ->sum('value');
-        return ['type_name' => self::$record_text[$type], 'sum' => intval($sum), 'unit' => self::$unit[$type]];
+        return ['type_name' => Absort::$record_text[$type], 'sum' => intval($sum), 'unit' => Absort::$unit[$type]];
     }
 
     private static function add(array $data)

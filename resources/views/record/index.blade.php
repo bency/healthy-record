387e6d3d2fb346adedc42418d1f48d363c57376b
bency <bency80097@gmail.com>
2016-03-06 14:39:44 +0800

    將各種數值分欄位顯示並計算總和

--- resources/views/record/index.blade.php
+++ resources/views/record/index.blade.php
@@ -1,6 +1,7 @@
 @inject('Record', 'App\Services\Record')
 @extends('common.base')
 @section('content')
+<?php $sum = [1 => 0, 2 => 0, 101 => 0, 102 => 0];?>
 <table class="table">
     <thead>
         <tr>
@@ -8,19 +9,31 @@
         </tr>
         <tr>
             <th>時間</th>
-            <th>項目</th>
-            <th>數量</th>
+            <th>喝水</th>
+            <th>吃飯</th>
+            <th>小號</th>
+            <th>大號</th>
             <th>登記</th>
         </tr>
     </thead>
     <tbody>
         @foreach($Record::getTodayRecords() as $r)
+        <?php
+            $sum[$r->attribute_id] += $r->value;
+         ?>
         @if (0 < $r->modified_at)
         <tr class="info">
+        @else
+        <tr>
         @endif
             <td>{{$r->getShortTime()}}</td>
-            <td>{{$r->getName()}}</td>
+            @foreach ([1, 2, 101, 102] as $niddle)
+                @if ($niddle == $r->attribute_id)
             <td>{{ intval($r->value)}} {{$r->getUnit()}}</td>
+                @else
+            <td></td>
+                @endif
+            @endforeach
             @if (0 < $r->modified_at)
             <td>已登記</td>
             @else
@@ -29,8 +42,15 @@
         </tr>
         @endforeach
         <tr>
+            <td>小計</td>
+            <td>{{$sum[1]}} 公克</td>
+            <td>{{$sum[2]}} 公克</td>
+            <td>{{$sum[101]}} 公克</td>
+            <td>{{$sum[102]}} 次</td>
+        </tr>
+        <tr>
             <td>總結</td>
-            <td colspan="3">今天到目前為止增加了 {{$Record::getSummaryByDay()}} 公克</td>
+            <td colspan="5">今天到目前為止增加了 {{$Record::getSummaryByDay()}} 公克</td>
         </tr>
     </tbody>
 </table>
